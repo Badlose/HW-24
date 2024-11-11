1844475d4d350d526cc114527e8c31c8c3d69891
Badlose <git@github.com:Badlose/helloworld.git>
2024-11-11 18:53:43 +0300

    Initial commit

---
+++ skypro-internet-store/src/main/java/pro/sky/skypro_internet_store/SkyproInternetStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro_internet_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproInternetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproInternetStoreApplication.class, args);
+	}
+
+}
